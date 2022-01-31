9256e282803347bc4e6adf95eca2a77cd30a5a44
Juan Rodenas <95724863+JuanRodenas@users.noreply.github.com>
2022-01-31 11:03:52 +0100

    Update backupsystem.sh

--- backupsystem.sh
+++ backupsystem.sh
@@ -25,9 +25,9 @@ ID="YOUR_TOKEN_CHAT"
 URL="https://api.telegram.org/bot$TOKEN/sendMessage"
 DNS="1.1.1.1"
 servidor=$(hostname)
-MSG="\U1F4BE Nasacloud informa \U2139"
+MSG="\U1F4BE $servidor informa \U2139"
 IP=`w -h | awk '{print""$3}'`
-mensaje="\U2139 Nasacloud informa que ha realizado la copia de seguridad mensual y se han eliminado los backups \U1F5AB más antiguos del servidor"
+mensaje="\U2139 $servidor informa que ha realizado la copia de seguridad mensual y se han eliminado los backups \U1F5AB más antiguos del servidor"
 
 # Envío del mensaje
 /usr/bin/ping -c2 $DNS > /dev/null 2>&1
