a510a643e6278b23a43dbcf436333f833014d88d
Juan Rodenas <95724863+JuanRodenas@users.noreply.github.com>
2022-01-31 09:23:09 +0100

    Create backupsystem.sh

---
+++ backupsystem.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+# Script de Juan Rodenas Sánchez
+# Script copias de seguridad regular del sistema
+
+# Accedemos a la carpeta docker
+cd /
+
+# Realizamos copia de seguridad de la carpeta home
+sleep 5 && sudo tar -cvzf /backup/home_$(date +%A-%d-%m-%Y).tar.gz home
+
+# Realizamos copia de seguridad de la carpeta root
+sleep 5 && sudo tar -cvzf /backup/root_$(date +%A-%d-%m-%Y).tar.gz root
+
+# Realizamos copia de seguridad del sistema
+EXCLUDE="--exclude=/mnt/* --exclude=/proc/* --exclude=/sys/* --exclude=/tmp/* --exclude=/backup/*"
+DATE=$(date +%A-%d-%m-%Y)
+sleep 5 && sudo tar -cvzf /backup/system_$DATE.tar.gz -X $EXCLUDE /
+
+# Borramos copias de seguridad más antiguas
+sudo find /backup* -mtime +60 -type f -exec rm {} \;
+
+# requeriments:
+TOKEN="YOUR_TOKEN_BOT"
+ID="YOUR_TOKEN_CHAT"
+URL="https://api.telegram.org/bot$TOKEN/sendMessage"
+DNS="1.1.1.1"
+servidor=$(hostname)
+MSG="\U1F4BE Nasacloud informa \U2139"
+IP=`w -h | awk '{print""$3}'`
+mensaje="\U2139 Nasacloud informa que ha realizado la copia de seguridad mensual y se han eliminado los backups \U1F5AB más antiguos del servidor"
+
+# Envío del mensaje
+/usr/bin/ping -c2 $DNS > /dev/null 2>&1
+if [ $? -ne 0 ]
+then
+        exit 0
+else
+        curl -s -X POST $URL \
+        -d chat_id=$ID \
+        -d parse_mode=HTML \
+        -d text="$(printf "$MSG<code>\n\t\t- Host: $servidor\n\t\t- IP: $IP\n\t\t- mensaje: $mensaje</code>")" \
+                > /dev/null 2>&1
+        exit 0
+fi
